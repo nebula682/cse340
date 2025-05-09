c0f984d6848102cd9e3e3532a240002bf4f0e59a
nebula682 <malcolmnie@gmail.com>
2025-05-09 09:30:37 +0200

    changes

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  *******************************************/
 /* ***********************
  * Require Statements
- *************************/
+ *************************
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
@@ -12,7 +12,7 @@ const app = express()
 const static = require("./routes/static")
 /* ***********************
  * View Engine and Templates
- *************************/
+ *************************
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
@@ -21,7 +21,7 @@ app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
- *************************/
+ *************************
 app.use(static)
 
 //index route
@@ -32,13 +32,64 @@ app.get("/", function(req, res){
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
- *************************/
+ *************************
 const port = process.env.PORT
 const host = process.env.HOST
 
 /* ***********************
  * Log statement to confirm server operation
- *************************/
+ *************************
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
+})
+
+/******************************************
+ * This server.js file is the primary file of the 
+ * application. It is used to control the project.
+ *******************************************/
+
+/* ***********************
+ * Require Statements
+ *************************/
+const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
+const env = require("dotenv").config()
+const path = require("path")
+const static = require("./routes/static")
+
+const app = express()
+
+/* ***********************
+ * Static Files Middleware - THIS WAS MISSING
+ *************************/
+app.use(express.static(path.join(__dirname, "public")))
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // assuming you have layouts/layout.ejs
+
+/* ***********************
+ * Routes
+ *************************/
+app.use(static)
+
+// index route
+app.get("/", function(req, res){
+  res.render("index", { title: "Home" })
+})
+
+/* ***********************
+ * Local Server Information
+ *************************/
+const port = process.env.PORT
+const host = process.env.HOST
+
+/* ***********************
+ * Start Server
+ *************************/
+app.listen(port, () => {
+  console.log(`App listening on ${host}:${port}`)
 })
